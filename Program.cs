373a053a8e6881bc276b4b9eb622b1251f70d7d3
Лейла Исмаилова <Trupeshnitzia@gmail.com>
2022-10-02 11:58:37 +0300

    добавлен вариант2

--- Program.cs
+++ Program.cs
@@ -7,13 +7,33 @@
 5 -> 1, 8, 27, 64, 125 
 */ 
  
-Console.WriteLine("Введите число");
-int userNumber = Convert.ToInt32(Console.ReadLine());
-int result = 1;
-double resultUser = 0;
-while (resultUser > Math.Pow(userNumber, 2))
+Console.Clear();  
+int UserNumber(string userInformation)
 {
-resultUser = Math.Pow(result, 2);
-Console.Write($"{resultUser} ");
-result++;
+    int result = 0;
+    Console.Write($"{userInformation} ");
+    while (!int.TryParse(Console.ReadLine(), out result) || result <= 1)
+    {
+        Console.Write($"Ошибка ввода! Введите целое число больше единицы. {userInformation} ");
+    }
+    return result;
+}
+
+int number = UserNumber("Введите целое число больше единицы: ");
+
+int[] tableCubeNumber(int number)
+{
+    int[] result = new int [number];
+    for(int i = 1; i <= number; i++)
+    {
+        result[i - 1] = (int)(Math.Pow(i, 3));
+    }
+    return result;
+}
+
+int[] table = tableCubeNumber(number);
+
+foreach (int value in table)
+{
+    Console.WriteLine(value + " ");
 }
